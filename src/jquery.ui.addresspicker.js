9af43ad70b2df2578036521bbb37f1515c0c8957
Gabriel Bedard Sice <gbedardsice@gmail.com>
2012-09-24 09:54:30 -0400

    Added administrative_area_level_1 to options (for province/state)

--- src/jquery.ui.addresspicker.js
+++ src/jquery.ui.addresspicker.js
@@ -20,8 +20,8 @@
         draggableMarker: true,
         regionBias: null,
         mapOptions: {
-            zoom: 5, 
-            center: new google.maps.LatLng(46, 2), 
+            zoom: 5,
+            center: new google.maps.LatLng(46, 2),
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         },
@@ -38,7 +38,7 @@
     marker: function() {
       return this.gmarker;
     },
-    
+
     map: function() {
       return this.gmap;
     },
@@ -46,29 +46,30 @@
     updatePosition: function() {
       this._updatePosition(this.gmarker.getPosition());
     },
-    
+
     reloadPosition: function() {
       this.gmarker.setVisible(true);
       this.gmarker.setPosition(new google.maps.LatLng(this.lat.val(), this.lng.val()));
       this.gmap.setCenter(this.gmarker.getPosition());
     },
-    
+
     selected: function() {
       return this.selectedResult;
     },
-    
+
     _create: function() {
       this.geocoder = new google.maps.Geocoder();
       this.element.autocomplete({
-        source: $.proxy(this._geocode, this),  
+        source: $.proxy(this._geocode, this),
         focus:  $.proxy(this._focusAddress, this),
         select: $.proxy(this._selectAddress, this)
       });
-      
+
       this.lat      = $(this.options.elements.lat);
       this.lng      = $(this.options.elements.lng);
       this.locality = $(this.options.elements.locality);
       this.country  = $(this.options.elements.country);
+      this.administrative_area_level_1 = $(this.options.elements.administrative_area_level_1);
       this.type     = $(this.options.elements.type);
       if (this.options.elements.map) {
         this.mapElement = $(this.options.elements.map);
@@ -83,13 +84,13 @@
 
       this.gmap = new google.maps.Map(this.mapElement[0], this.options.mapOptions);
       this.gmarker = new google.maps.Marker({
-        position: this.options.mapOptions.center, 
-        map:this.gmap, 
+        position: this.options.mapOptions.center,
+        map:this.gmap,
         draggable: this.options.draggableMarker});
       google.maps.event.addListener(this.gmarker, 'dragend', $.proxy(this._markerMoved, this));
       this.gmarker.setVisible(false);
     },
-    
+
     _updatePosition: function(location) {
       if (this.lat) {
         this.lat.val(location.lat());
@@ -98,11 +99,11 @@
         this.lng.val(location.lng());
       }
     },
-    
+
     _markerMoved: function() {
       this._updatePosition(this.gmarker.getPosition());
     },
-    
+
     // Autocomplete source method: fill its suggests with google geocoder results
     _geocode: function(request, response) {
         var address = request.term, self = this;
@@ -114,11 +115,11 @@
                 for (var i = 0; i < results.length; i++) {
                     results[i].label =  results[i].formatted_address;
                 };
-            } 
+            }
             response(results);
         })
     },
-    
+
     _findInfo: function(result, type) {
       for (var i = 0; i < result.address_components.length; i++) {
         var component = result.address_components[i];
@@ -128,13 +129,13 @@
       }
       return false;
     },
-    
+
     _focusAddress: function(event, ui) {
       var address = ui.item;
       if (!address) {
         return;
       }
-      
+
       if (this.gmarker) {
         this.gmarker.setPosition(address.geometry.location);
         this.gmarker.setVisible(true);
@@ -142,18 +143,21 @@
         this.gmap.fitBounds(address.geometry.viewport);
       }
       this._updatePosition(address.geometry.location);
-      
+
       if (this.locality) {
         this.locality.val(this._findInfo(address, 'locality'));
       }
       if (this.country) {
         this.country.val(this._findInfo(address, 'country'));
       }
+      if (this.administrative_area_level_1) {
+        this.administrative_area_level_1.val(this._findInfo(address, 'administrative_area_level_1'))
+      }
       if (this.type) {
         this.type.val(address.types[0]);
       }
     },
-    
+
     _selectAddress: function(event, ui) {
       this.selectedResult = ui.item;
     }
